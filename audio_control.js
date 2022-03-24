4a7dc74b6e1a5340575cfa10f6c7af9754f0feb2
BiliBili-wulihb <102204002+BiliBili-wulihb@users.noreply.github.com>
2022-03-24 19:29:34 +0800

    Update audio_control.js

--- audio_control.js
+++ audio_control.js
@@ -1,2 +1,2 @@
 /*Background-music*/
-document.write('<audio autoplay="autoplay" controls="controls" loop="loop" preload="auto" src="music_summertime.mp3"></audio>')
+document.write('<audio autoplay="autoplay" controls="controls" loop="loop" preload="auto" src="music_summertime.mp3" class="background-music"></audio>')
